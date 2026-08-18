232493d133a7cb13bb73eb43d2f2d749c298d854
mmongelli5 <mmongelli@studenti.uninsubria.it>
2026-08-18 21:31:48 +0200

    -creata la DataClass DatiMelanoma per rappresentare lo stato del melanoma del paziente, secondo i dati rilevanti per la classificazione TNM. -aggiunta relativa documentazione

---
+++ app/src/main/java/com/example/melanomatnm/model/DatiMelanoma.kt
@@ -0,0 +1,16 @@
+package com.example.melanomatnm.model
+
+/**
+ * Classe per rappresentare i parametri rilevanti per la classificazione del melanoma
+ * @property spessoreBreslow: Spessore di Breslow, ovvero la profondità in mm del melanoma
+ * @property ulcerazione: Indica se è presente o meno ulcerazione del melanoma
+ * @property numeroLinfonodi: Numero di linfonodi regionali in metastasi
+ * @property metastasi: Indica se è presente o meno metastasi a distanza
+ */
+data class DatiMelanoma(
+    val spessoreBreslow: Double,
+    val ulcerazione: Boolean,
+    val numeroLinfonodi: Int,
+    val metastasi: Boolean,
+)
+
--- app/src/main/java/com/example/melanomatnm/view/MainActivity.kt
+++ app/src/main/java/com/example/melanomatnm/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.melanomatnm
+package com.example.melanomatnm.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
